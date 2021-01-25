9a7e5a2b7860b9516ed2fbaba0baa7ec8959433e
Tobi Shitta-Bey <tsb99@Tobis-MacBook-Pro-2.local>
2021-01-25 10:47:49 +0000

    feat[swr-impl]:swr boilerplate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
 import "./App.css";
 
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 function App() {
   const [query, setQuery] = React.useState("");
   const [count, setCount] = React.useState(0);
   const [caughtCount, setCaughtCount] = React.useState(0);
 
+  const { data, error } = useSWR(
+    `https://api.github.com/users/${query}`,
+    fetcher
+  );
+
   const handleRequest = (counter) => {
     console.log(count, caughtCount);
     caughtCount === count && caughtCount != 0 && count != 0
-      ? console.log("Calling from GitHub")
+      ? (
+        console.log("Calling from GitHub")
+      )
       : console.log("never mind");
 
     setCount(1);
@@ -28,15 +39,27 @@ function App() {
     console.log(query);
   };
 
+  if (error) return <h1>error</h1>
+
   return (
     <div className="App">
       <input
         type="text"
         name="song"
-        // placeholder="Search for an album..."
+        placeholder="Search for GitHub users.."
         value={query}
         onChange={(e) => handleTextChange(e)}
       />
+      {/* <div className="divider"/> */}
+
+      <div className="drop-down-item-container">
+        <div className="image-container">
+          {/* <img src={data.avatar_url} className="profile-image" /> */}
+        </div>
+        <div className = "user-name-container">
+          <h1>{JSON.stringify(data)}</h1>
+        </div>
+      </div>
     </div>
   );
 }
